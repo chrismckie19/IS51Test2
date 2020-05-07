58854308880cbb2ddd53a14f2378e52cd3827458
chrismckie19 <60371451+chrismckie19@users.noreply.github.com>
2020-05-06 20:51:05 -0700

    finished exam

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -14,4 +14,9 @@ export class MainNavComponent implements OnInit {
   ngOnInit() {
   }
 
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application is designed by Chris McKie. (C) 2020');
+  }
+
+
 }
